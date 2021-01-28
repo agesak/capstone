3b319151b59446a2b36a9dbe795b0d86af01fc3c
Kareha Agesa <kmaagesa@gmail.com>
2021-01-27 22:08:07 -0800

    Initial Commit

---
+++ gymbuddies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gymbuddies
+//
+//  Created by Kareha on 1/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
