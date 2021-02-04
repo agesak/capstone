cc097accd0a3f5715739ee032fcfc1f6ebd03b54
Kareha Agesa <kmaagesa@gmail.com>
2021-02-04 12:23:47 -0800

    separate demographic info page

--- gymbuddies/CreatePreferencesView.swift
+++ gymbuddies/CreatePreferencesView.swift
@@ -14,16 +14,7 @@ struct CreatePreferencesView: View {
     @State var name : String = ""
     @State var age : String = ""
     @State var location : String = ""
-//    @State var pronouns : String = ""
-    
-    @State var times = ["Morning", "Afternoon", "Evening"]
-    @State private var timesIndex = 0
-    
-    @State var style = ["HIIT", "Crossfit", "Running", "Yoga"]
-    @State private var styleIndex = 0
-    
-    @State var frequency = ["1x/week", "2x/week", "3x/week", "4x/week", "5-6x/week"]
-    @State private var frequencyIndex = 0
+    @State var pronouns : String = ""
     
     @State private var isprofileValid: Bool = false
     @State private var shouldShowProfileAlert: Bool = false
@@ -31,49 +22,29 @@ struct CreatePreferencesView: View {
     private var db = Firestore.firestore()
     
     var body: some View {
-//        NavigationView{
-            VStack {
-//                    if user != nil {
-//                    Text(user!.email!)
-//                    Text(user!.uid)
-//                }
-                
-                
-                Form {
-                    
-                        Section(header: Text("Demographic Information")) {
-                            TextField("Name", text: $name)
-                            TextField("Age", text: $age)
-                            TextField("Location", text: $location)
-//                            TextField("Pronouns", text: $pronouns)
-                        }
+        VStack() {
+            Spacer()
+            
+//            Group {
+                FieldView(fieldName: "Name", fieldString: "Michelle Obama", stateVar: self.$name)
+                FieldView(fieldName: "Age", fieldString: "25", stateVar: self.$age)
+                FieldView(fieldName: "Location", fieldString: "Seattle, WA", stateVar: self.$location)
+                FieldView(fieldName: "Pronouns", fieldString: "she/her", stateVar: self.$pronouns).autocapitalization(.none)
 
-                    
-                        Section(header: Text("Workout Preferences")) {
-                                Picker(selection: $timesIndex, label: Text("Time of Day")) {
-                                    ForEach(0 ..< times.count) {
-                                        Text(self.times[$0])
-                                    }
-                                }
-                            
-                            Picker(selection: $styleIndex, label: Text("Preferred Workout Style")) {
-                                ForEach(0 ..< style.count) {
-                                    Text(self.style[$0])
-                                }
-                            }
-                            
-                            Picker(selection: $frequencyIndex, label: Text("Workout Frequency")) {
-                                ForEach(0 ..< frequency.count) {
-                                    Text(self.frequency[$0])
-                                }
-                            }
-                        }.padding()
-                        
-                    
-                }.navigationBarTitle("Profile Information")
+            Spacer().frame(height: 75)
+//            }
+//                Form {
+//
+//                        Section(header: Text("Demographic Information")) {
+//                            TextField("Name", text: $name)
+//                            TextField("Age", text: $age)
+//                            TextField("Location", text: $location)
+////                            TextField("Pronouns", text: $pronouns)
+//                        }
+
+                }.navigationBarTitle("First Tell Me Your...")
                 
                 NavigationLink(
-//                    will likely go to photo upload view
                     destination: UsersView(),
                     isActive: self.$isprofileValid) {
                         Text("Continue")
@@ -89,17 +60,9 @@ struct CreatePreferencesView: View {
                                     "name": self.name,
                                     "age": self.age,
                                     "location": self.location,
-                                    "times": self.times[self.timesIndex],
-                                    "style": self.style[self.styleIndex],
-                                    "frequency": self.frequency[self.frequencyIndex],
                                     "email": user?.email!
                                 ]
-                                
-//                                let docRef = db.collection("users").document("\(user!.uid)")
                                 let docRef = Firestore.firestore().document("users/\(user!.uid)")
-                                
-//                                let docRef = Firestore.firestore().document("users/\(user!.uid)")
-                                print("setting data")
                                 docRef.setData(userDictionary as [String : Any]){ (error) in
                                         if let error = error {
                                             print("error = \(error)")
@@ -109,12 +72,12 @@ struct CreatePreferencesView: View {
                                             self.isprofileValid = true
                                         }
                                     }
-                                
                             }
                     }
                 Spacer()
                 
-            }.alert(isPresented: $shouldShowProfileAlert) {
+//            }
+    .alert(isPresented: $shouldShowProfileAlert) {
                 Alert(title: Text("Error Creating Profile"))}
             .navigationBarBackButtonHidden(true)
         }
@@ -123,5 +86,23 @@ struct CreatePreferencesView: View {
 struct CreatePreferencesView_Previews: PreviewProvider {
     static var previews: some View {
         CreatePreferencesView()
+    }
+}
+
+
+struct FieldView : View {
+    
+    var fieldName: String
+    var fieldString: String
+    @Binding var stateVar: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+        Spacer()
+            Text(fieldName).font(.title).fontWeight(.medium)
+            TextField(fieldString, text: $stateVar)
+        Spacer()
+        }
+        .padding(.leading)
     }
 }
