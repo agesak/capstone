5cd57b880fb05b930c8ba60b2a5442db934ce77c
Kareha Agesa <kmaagesa@gmail.com>
2021-02-05 12:43:23 -0800

    change app icon, home screen buttons now blue

--- gymbuddies/HomeView.swift
+++ gymbuddies/HomeView.swift
@@ -11,59 +11,74 @@ import Firebase
 
 struct HomeView : View {
     
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+    
     var body : some View {
         NavigationView {
             
-            VStack {
-                Spacer()
-                AppName()
-                Spacer()
-                AppLogo()
-                Spacer()
-            
+            ZStack{
+
+                if colorScheme == .dark {
+                    Image("barbell_2nd_lighter").resizable().aspectRatio(contentMode: .fill).ignoresSafeArea()
+                } else {
+                    Image("barbell").resizable().aspectRatio(contentMode: .fill).ignoresSafeArea().opacity(0.1)
+                }
+                VStack {
+                    Spacer()
+                    AppName()
+                    Spacer()
+                    AppLogo()
+                    Spacer()
+                
                  NavigationLink(
-                    destination: CreateAccountView(),
-                    label: {
-                        Text("Sign Up")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .padding()
-                      .frame(width: 250, height: 50)
-        //                for custom divide rbg by 255
-                        .background(Color.purple)
-                      .cornerRadius(10.0)
-                    })
-                
-            NavigationLink(
-               destination: SignInView(),
-               label: {
-                   Text("Sign In")
-                   .font(.title)
-                   .fontWeight(.bold)
-                   .foregroundColor(.white)
-                   .padding()
-                 .frame(width: 250, height: 50)
-   //                for custom divide rbg by 255
-                   .background(Color.purple)
-                 .cornerRadius(10.0)
-               })
-                
-                Spacer()
+                        destination: CreateAccountView(),
+                        label: {
+                            Text("Sign Up")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding()
+                          .frame(width: 250, height: 50)
+            //                for custom divide rbg by 255
+//                                .background(Color.purple)
+                                .background(Color(red: 135.0 / 255.0, green: 206.0 / 255.0, blue: 250.0 / 255.0))
+//                                .background(Color(red: 255.0 / 255.0, green: 153.0 / 255.0, blue: 153.0 / 255.0))
+                          .cornerRadius(10.0)
+                        })
+                    
+                NavigationLink(
+                   destination: SignInView(),
+                   label: {
+                       Text("Sign In")
+                       .font(.title)
+                       .fontWeight(.bold)
+                       .foregroundColor(.white)
+                       .padding()
+                     .frame(width: 250, height: 50)
+       //                for custom divide rbg by 255
+                       .background(Color(red: 135.0 / 255.0, green: 206.0 / 255.0, blue: 250.0 / 255.0))
+                     .cornerRadius(10.0)
+                   })
+                    
+                    Spacer()
+                }
             }
         }.navigationBarBackButtonHidden(true)
     }
 }
 
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .preferredColorScheme(.light)
     }
 }
 
 struct AppName: View {
     var body: some View {
-        Text("Gym Buddies")
+        Text("MyGymPals")
             .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
             .fontWeight(.bold)
             .foregroundColor(Color.black)
@@ -74,9 +89,10 @@ struct AppLogo: View {
     var body: some View {
         //                cant use sfsymbols as logos?
         //                sportscourt.fill
-        Image(systemName: "sportscourt")
-            .padding(.top)
-            .font(.system(size: 65, weight: .light))
+        Image("barbell-icon")
+//        Image(systemName: "sportscourt")
+//            .padding(.top)
+//            .font(.system(size: 65, weight: .light))
     }
 }
 
