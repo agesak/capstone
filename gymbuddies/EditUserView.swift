5a087810c851151172972fd422f2f6e98a590613
Kareha Agesa <kmaagesa@gmail.com>
2021-02-08 12:50:59 -0800

    separate edit field structure

--- gymbuddies/EditUserView.swift
+++ gymbuddies/EditUserView.swift
@@ -7,38 +7,77 @@
 
 import SwiftUI
 
+
+
+
 struct EditUserView: View {
     
     var currentUser : User
     @State var name : String = ""
+    @State var pronouns : String = ""
+//    @State var about_me : String = ""
+    @State var style : String = ""
+    @State var frequency : String = ""
+    @State var times : String = ""
     
-//    var model:String    // Actual a more complex view model
-//    @State var editedValue: String
-//
-//    init(model: String) {
-//        self.model = model
-//        self._editedValue = State(wrappedValue: model) // _editedValue is State<String>
-//    }
-    
-//    name: String
     init(currentUser: User){
         self.currentUser = currentUser
-//        self.name = name
-//        self._show = show
-//        self.toUser = toUser
     }
     
     var body: some View {
         
-        
-        Text("Name").font(.headline).fontWeight(.light)
-        TextField("Enter your Email Address", text: $name)
-            .onAppear {self.name = currentUser.name}
+        VStack(alignment: .leading){
+            
+            EditFieldView(fieldName: "Name:", user: currentUser, stateVar: self.$name, defaultVal: currentUser.name)
+            EditFieldView(fieldName: "Pronouns:", user: currentUser, stateVar: self.$pronouns, defaultVal: currentUser.pronouns)
+            EditFieldView(fieldName: "Workout Style:", user: currentUser, stateVar: self.$style, defaultVal: currentUser.style)
+            EditFieldView(fieldName: "Preferred Frequency:", user: currentUser, stateVar: self.$frequency, defaultVal: currentUser.frequency)
+            EditFieldView(fieldName: "Preferred Time:", user: currentUser, stateVar: self.$times, defaultVal: currentUser.times)
+            
+//            HStack {
+//            Text("Name").font(.headline).fontWeight(.bold)
+//            TextField("", text: $name)
+//                .onAppear {self.name = currentUser.name}
+//            }
+//            .padding(.leading)
+//            Divider()
+//                .padding(.horizontal)
+
+            
+//            to do- icon
+            
+            
+        }
     }
 }
 
-//struct EditUserView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        EditUserView()
-//    }
-//}
+struct EditFieldView : View {
+    
+    var fieldName: String
+    var user : User
+    @Binding var stateVar: String
+    var defaultVal: String
+    
+    var body: some View {
+        
+        HStack {
+            Text(fieldName).font(.title2).fontWeight(.bold)
+            TextField("", text: $stateVar)
+                .onAppear {stateVar = defaultVal}
+                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+        }.padding(.leading)
+        
+        Divider().padding(.horizontal)
+        
+//        Spacer()
+    }
+}
+
+
+struct EditUserView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditUserView(currentUser: User(id: "", age: "30", name: "Michelle Obama", location: "Seattle, WA", pronouns: "(she/her)", frequency: "4x/week", style: "Crossfit", times: "Evening", pic: ""))
+    }
+}
+
+
