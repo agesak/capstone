249ece537f6247f3fea22cba637c94b2de6c4c02
Kareha Agesa <kmaagesa@gmail.com>
2021-02-08 11:19:18 -0800

    remove old messageview file

---
+++ gymbuddies/EditUserView.swift
@@ -0,0 +1,20 @@
+//
+//  EditUserView.swift
+//  gymbuddies
+//
+//  Created by Kareha on 2/8/21.
+//
+
+import SwiftUI
+
+struct EditUserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditUserView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditUserView()
+    }
+}
--- gymbuddies/OtherUserView.swift
+++ gymbuddies/OtherUserView.swift
@@ -179,31 +179,3 @@ struct MainOtherUserView: View {
         }
     }
 }
-
-//struct OtherUserView: View {
-//    
-//    var toUser:User
-//    
-//    var body: some View {
-//        VStack{
-//            Text(toUser.name)
-//            
-//            URLImage(url: URL(string: toUser.pic)!) { image in
-//                image
-//                    .resizable()
-//                    .aspectRatio(contentMode: .fit)
-//            }.frame(width: 100.0, height: 100.0)
-//            
-//            NavigationLink(
-//                destination: MessageView(toUser: toUser),
-//                label: {Text("Message")}
-//            )
-//        }
-//    }
-//}
-
-//struct OtherUserView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        OtherUserView(user: user.name)
-//    }
-//}
