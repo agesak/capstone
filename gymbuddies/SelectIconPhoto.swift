50fcf2e460f2f61ed1b47879762ec61cc282a85b
Kareha Agesa <kmaagesa@gmail.com>
2021-02-11 00:03:54 -0800

    adding more icons to pick from

--- gymbuddies/SelectIconPhoto.swift
+++ gymbuddies/SelectIconPhoto.swift
@@ -21,12 +21,20 @@ struct SelectIconPhoto: View {
     @State var selectedPhoto : String = ""
     
     let user = Auth.auth().currentUser
-    let catUrl = "https://gymbuddiescapstone.s3-us-west-1.amazonaws.com/cat.png"
-    let pengiunUrl = "https://gymbuddiescapstone.s3-us-west-1.amazonaws.com/pengiun.png"
+    let dogSunglassesURL = "https://gymbuddiescapstone.s3-us-west-1.amazonaws.com/dog-sunglasses.jpg"
+    let pugUrl = "https://gymbuddiescapstone.s3-us-west-1.amazonaws.com/pug.jpg"
+    let puppyMugUrl = "https://gymbuddiescapstone.s3-us-west-1.amazonaws.com/puppy-mug.jpg"
+    let puppyToothsUrl = "https://gymbuddiescapstone.s3-us-west-1.amazonaws.com/puppyTooths.jpg"
+    let twoRetrieversUrl = "https://gymbuddiescapstone.s3-us-west-1.amazonaws.com/two-retrievers.jpg"
     
-    func setPhotoUrl(url: String) {
-        self.selectedPhoto = url
-    }
+    let kbsUrl = "https://gymbuddiescapstone.s3-us-west-1.amazonaws.com/kbs.jpg"
+    let pinkKbsUrl = "https://gymbuddiescapstone.s3-us-west-1.amazonaws.com/pink-kbs.jpg"
+    let rougueUrl = "https://gymbuddiescapstone.s3-us-west-1.amazonaws.com/rougue-vibes.jpg"
+//    let dumbbellsUrl = "https://gymbuddiescapstone.s3-us-west-1.amazonaws.com/dumbbells.jpg"
+    let glovesUrl = "https://gymbuddiescapstone.s3-us-west-1.amazonaws.com/gloves.jpg"
+    let yogaVibesUrl = "https://gymbuddiescapstone.s3-us-west-1.amazonaws.com/yoga.jpg"
+    
+
     
  
     var body: some View {
@@ -42,27 +50,41 @@ struct SelectIconPhoto: View {
             
             VStack {
                 
-                VStack{
+                Text("Adorable Puppies")
+                    .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.semibold/*@END_MENU_TOKEN@*/)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading)
+                ScrollView(.horizontal, showsIndicators: true){
+                    HStack{
                     
-                    Button(action: {setPhotoUrl(url: catUrl)}) {
-                        URLImage(url: URL(string: catUrl)!) { image in
-                            image
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                        }.frame(width: 100.0, height: 100.0)
-                    }.background(self.selectedPhoto == catUrl ? Color.blue : Color.clear)
+                        IconView(photoUrl: puppyMugUrl, heightSize: 100.0, selectedPhoto: self.$selectedPhoto)
+                        IconView(photoUrl: dogSunglassesURL, heightSize: 100.0, selectedPhoto: self.$selectedPhoto)
+                        IconView(photoUrl: pugUrl, heightSize: 100.0, selectedPhoto: self.$selectedPhoto)
+                        IconView(photoUrl: twoRetrieversUrl, heightSize: 100.0, selectedPhoto: self.$selectedPhoto)
+                        IconView(photoUrl: puppyToothsUrl, heightSize: 100.0, selectedPhoto: self.$selectedPhoto)
+                    }.padding(.horizontal)
+
+                }
+                
+                VStack {
                     
-                    
-                    Button(action: {setPhotoUrl(url: pengiunUrl)}) {
-                        URLImage(url: URL(string: pengiunUrl)!) { image in
-                            image
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                        }.frame(width: 100.0, height: 100.0)
-                    }.background(self.selectedPhoto == pengiunUrl ? Color.gray : Color.clear)
-                    
-    //                Image("cat").resizable().scaledToFit().frame(width: 100.0, height: 100.0)
-    //                Image("pengiun").resizable().scaledToFit().frame(width: 100.0, height: 100.0)
+                    Text("Workout")
+                        .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.semibold/*@END_MENU_TOKEN@*/)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading)
+                    ScrollView(.horizontal, showsIndicators: true){
+                        HStack{
+                        
+                            IconView(photoUrl: kbsUrl, heightSize: 200.0, selectedPhoto: self.$selectedPhoto)
+                            IconView(photoUrl: pinkKbsUrl, heightSize: 150.0, selectedPhoto: self.$selectedPhoto)
+                            IconView(photoUrl: rougueUrl, heightSize: 150.0, selectedPhoto: self.$selectedPhoto)
+                            IconView(photoUrl: glovesUrl, heightSize: 100.0, selectedPhoto: self.$selectedPhoto)
+                            IconView(photoUrl: yogaVibesUrl, heightSize: 150.0, selectedPhoto: self.$selectedPhoto)
+                        }.padding(.horizontal)
+
+                    }
                 }
                 
                 Spacer().frame(height: 90)
@@ -102,5 +124,32 @@ struct SelectIconPhoto: View {
 struct SelectIconPhoto_Previews: PreviewProvider {
     static var previews: some View {
         SelectIconPhoto()
+    }
+}
+
+
+struct IconView : View{
+    
+    var photoUrl : String
+//    tbh can get rid of this....
+    var heightSize: CGFloat
+    @Binding var selectedPhoto : String
+    
+    func setPhotoUrl(url: String) {
+        self.selectedPhoto = url
+    }
+    
+    var body: some View {
+        
+        Button(action: {setPhotoUrl(url: photoUrl)}) {
+            URLImage(url: URL(string: photoUrl)!) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(5)
+//                    .frame(height: 500)
+            }.frame(width: 100.0, height: 100.0)
+        }.background(self.selectedPhoto == photoUrl ? Color(red: 135.0 / 255.0, green: 206.0 / 255.0, blue: 250.0 / 255.0) : Color.clear)
+        
     }
 }
