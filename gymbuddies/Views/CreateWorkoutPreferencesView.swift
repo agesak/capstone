9d77ef396d238649d6c5226552a0b6e8fa409722
Kareha Agesa <kmaagesa@gmail.com>
2021-02-13 16:20:56 -0800

    changed workout style page

--- gymbuddies/Views/CreateWorkoutPreferencesView.swift
+++ gymbuddies/Views/CreateWorkoutPreferencesView.swift
@@ -8,6 +8,39 @@
 import SwiftUI
 import Firebase
 
+struct SelectorView: View {
+
+    var fieldName : String
+    @Binding var stateListVar: [String]
+    @Binding var stateIndexVar: Int
+    @Binding var pickingVar : Bool
+
+    var body: some View {
+        HStack{
+            Text(fieldName).font(.title2).fontWeight(.bold).padding(.leading)
+            Spacer()
+            HStack{
+                Text(stateListVar[stateIndexVar])
+                Image(systemName: pickingVar ? "chevron.up" : "chevron.down").resizable().frame(width: 13, height: 6).padding(.trailing)
+            }.onTapGesture {
+                self.pickingVar.toggle()
+                print("tapped")
+                print(self.pickingVar)
+            }
+        }
+        if Bool(pickingVar){
+            Section {
+                Picker(selection: self.$stateIndexVar, label: Text("")) {
+                    ForEach(0 ..< stateListVar.count) {
+                        Text(self.stateListVar[$0])
+                    }
+                }
+            }
+        }
+        Divider().padding(.horizontal)
+    }
+}
+
 struct CreateWorkoutPreferencesView: View {
     
     @State var times = ["Morning", "Afternoon", "Evening"]
@@ -22,6 +55,10 @@ struct CreateWorkoutPreferencesView: View {
     @State private var isprofileValid: Bool = false
     @State private var shouldShowProfileAlert: Bool = false
     
+    @State var selectStyle = false
+    @State var selectTimes = false
+    @State var selectFrequency = false
+    
     let user = Auth.auth().currentUser
     
     init(){
@@ -32,40 +69,51 @@ struct CreateWorkoutPreferencesView: View {
         
         ZStack{
             
-            Image("barbell-cropped").resizable().aspectRatio(contentMode: .fill).opacity(0.1).ignoresSafeArea()
+            Image("barbell-cropped").resizable().opacity(0.1).ignoresSafeArea()
+//            .aspectRatio(contentMode: .fill)
         
-            VStack{
-                Spacer().frame(height: 130)
-                Form{
-                    Section {
-                        Picker(selection: $styleIndex, label: Text("Preferred Workout Style")) {
-                            ForEach(0 ..< style.count) {
-                                Text(self.style[$0])
-                            }
-                        }
-                    }
-                    
-                    
-                    Section() {
-                            Picker(selection: $timesIndex, label: Text("When?")) {
-                                ForEach(0 ..< times.count) {
-                                    Text(self.times[$0])
-                                }
-                            }
-                        }
-                    
-                        
-                    Section{
-                        
-                        Picker(selection: $frequencyIndex, label: Text("How Often?")) {
-                            ForEach(0 ..< frequency.count) {
-                                Text(self.frequency[$0])
-                            }
-                        }
-                    }
-                }
-                .foregroundColor(Color.black)
-                .navigationBarTitle("Workout Style?")
+            VStack(){
+//                Spacer().frame(height: 50)
+                Text("Workout Style?")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading)
+                Spacer().frame(height: 100)
+                SelectorView(fieldName: "Style?", stateListVar: self.$style, stateIndexVar: self.$styleIndex, pickingVar: self.$selectStyle)
+                SelectorView(fieldName: "When?", stateListVar: self.$times, stateIndexVar: self.$timesIndex, pickingVar: self.$selectTimes)
+                SelectorView(fieldName: "How Often?", stateListVar: self.$frequency, stateIndexVar: self.$frequencyIndex, pickingVar: self.$selectFrequency)
+                Spacer().frame(height: 90)
+//                Form{
+//                    Section {
+//                        Picker(selection: $styleIndex, label: Text("Preferred Workout Style")) {
+//                            ForEach(0 ..< style.count) {
+//                                Text(self.style[$0])
+//                            }
+//                        }
+//                    }
+//
+//
+//                    Section{
+//                            Picker(selection: $timesIndex, label: Text("When?")) {
+//                                ForEach(0 ..< times.count) {
+//                                    Text(self.times[$0])
+//                                }
+//                            }
+//                        }
+//
+//
+//                    Section{
+//
+//                        Picker(selection: $frequencyIndex, label: Text("How Often?")) {
+//                            ForEach(0 ..< frequency.count) {
+//                                Text(self.frequency[$0])
+//                            }
+//                        }
+//                    }
+//                }
+//                .foregroundColor(Color.black)
+//                .navigationBarTitle("Workout Style?")
                 
                 NavigationLink(
                     destination: SelectIconPhotoView(),
@@ -89,7 +137,7 @@ struct CreateWorkoutPreferencesView: View {
                                     }
                             }
                     }
-                Spacer().frame(height: 150)
+                Spacer().frame(height: 100)
             }
         }.alert(isPresented: $shouldShowProfileAlert) {
             Alert(title: Text("Error Creating Profile"))}
